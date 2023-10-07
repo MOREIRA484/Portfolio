ba1fc1a8b92e257838215e8e300baa368ab818f1
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-10-07 02:33:44 -0300

    Initial Commit

---
+++ Portfolioo/PortfoliooApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfoliooApp.swift
+//  Portfolioo
+//
+//  Created by leonardo Moreira on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PortfoliooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
